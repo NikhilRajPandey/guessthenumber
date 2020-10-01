8f110a044f930c4e2532bc69b7796d7d54a90aa1
Kamal Kumar Jena <72182167+Kamal-Kumar-Jena@users.noreply.github.com>
2020-10-01 21:26:30 +0530

    Updated guessthenumber.py
    My program is a updated version of your program but just error proof. Merge this file into yours.

--- guessthenumber.py
+++ guessthenumber.py
@@ -1,34 +1,36 @@
-#this is the same code but its simplified and its easy for other coders to read 
-#you have used a lot of elif which is reduced
+# this is the same code but its simplified and its easy for other coders to read
+# you have used a lot of elif which is reduced
 
-n=18
-attempts=1
+n = 18
+attempts = 1
 
-while (attempts<=9):
+while (attempts <= 9):
+    try:
+        inp = int(input("Enter your number\n"))
 
-    inp=int(input("Enter your number\n"))
+        if inp > n:
 
-    if inp>n:
-
-         print("Enter a lesser number\n")
-
-
-    elif inp<n:
-
-         print("Enter a greater number\n")
+            print("Enter a lesser number\n")
 
 
-    else:
-         print("you win")
-         print("you finished the game in",attempts,"attempts" )
-         break
+        elif inp < n:
 
-    print(9 - attempts, "no. of guesses left")
-    attempts = attempts + 1
+            print("Enter a greater number\n")
 
-    if attempts>9:
-        print("game over")
 
-    if attempts==9:
-        print("Hint : Number is between 15 to 20")
+        else:
+            print("you win")
+            print("you finished the game in", attempts, "attempts")
+            break
+
+        print(9 - attempts, "no. of guesses left")
+        attempts = attempts + 1
+
+        if attempts > 9:
+            print("game over")
+
+        if attempts == 9:
+            print("Hint : Number is between 15 to 20")
+    except ValueError:
+        print("Enter a Integer only")
 
