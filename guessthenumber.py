30989f8181035ca1f23cb620483457c0b9b2c13e
Ankit Singh <67016832+Ankitsinghprograms@users.noreply.github.com>
2020-10-18 21:14:58 +0530

    MAKING GUESS THE NO GAME MORE INTERESTING
    I added random module which makes game more interesting and fstrings to make code faster and some User interface (UI) changes.

--- guessthenumber.py
+++ guessthenumber.py
@@ -1,36 +1,48 @@
 # this is the same code but its simplified and its easy for other coders to read
 # you have used a lot of elif which is reduced
+#Importing Random Module
 
-n = 18
+import random
+
+n = random.randint(1,20)
+
 attempts = 1
 
-while (attempts <= 9):
+while attempts <=9:
+
     try:
-        inp = int(input("Enter your number\n"))
+
+        inp = int(input("Guess Any number between 1 to 20\n"))
 
         if inp > n:
 
             print("Enter a lesser number\n")
 
-
         elif inp < n:
 
             print("Enter a greater number\n")
 
-
         else:
+
             print("you win")
-            print("you finished the game in", attempts, "attempts")
+
+            print(f"you finished the game in {attempts} attempts")
+
             break
 
-        print(9 - attempts, "no. of guesses left")
+        print(f"{9 - attempts} no. of guesses left")
+
         attempts = attempts + 1
 
         if attempts > 9:
+
             print("game over")
 
         if attempts == 9:
-            print("Hint : Number is between 15 to 20")
+
+            print(f"Hint : Number is between {n-3} to {n+1}")
+
     except ValueError:
+
         print("Enter a Integer only")
 
